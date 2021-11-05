132d92fb16ba188385261a6c5de0895f72e93a94
kryvetski-andrei <kryvetskistud@gmail.com>
2021-11-05 10:28:18 +0300

    feat: add 01-read-file solution

---
+++ 01-read-file/index.js
@@ -0,0 +1,8 @@
+const path = require('path');
+const fs = require('fs')
+const filePath = path.join(__dirname, 'text.txt');
+const readStream = fs.createReadStream(filePath);
+
+readStream.on('data', (chunk) => {
+  console.log(chunk.toString())
+})
