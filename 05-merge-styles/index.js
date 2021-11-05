c724c2f711a66ba64d147dbc613c5502611f5c01
kryvetski-andrei <kryvetskistud@gmail.com>
2021-11-05 23:00:36 +0300

    feat: add 05-merge-styles solution

---
+++ 05-merge-styles/index.js
@@ -0,0 +1,17 @@
+const path = require('path');
+const fs = require('fs');
+
+fs.readdir(path.join(__dirname, 'styles'), { withFileTypes: true }, (error, data) => {
+  error ?
+    console.log(error) :
+    data.forEach((file) => {
+      file.isFile() && path.extname(file.name) === '.css' ?
+        fs.readFile(path.join(__dirname, 'styles', file.name),  (error, data) => {
+          error ?
+            console.log(error) :
+            fs.writeFileSync(path.join(__dirname, 'project-dist', 'bundle.css'), `${data}`, (error) => {
+              error ? console.log(error) : null;
+            });
+        }) : null;
+    })
+}) 
